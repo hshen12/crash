6c0bea15f183bab3cda49437000a2bbecc092d43
hao <hshen12@dons.usfca.edu>
2019-04-06 19:58:50 -0700

    commit

--- shell.c
+++ shell.c
@@ -16,10 +16,15 @@
 #include "tokenizer.h"
 #include "leetify.h"
 
+#define JOB_LIST_SIZE 10
+
 int command_number;
 char username[LOGIN_NAME_MAX];
 char hostname[HOST_NAME_MAX];
 char cwd[PATH_MAX];
+pid_t pid_list[JOB_LIST_SIZE];
+int list_index;
+char *line_list[JOB_LIST_SIZE];
 
 /**
 * Print shell promt information
@@ -27,6 +32,17 @@ char cwd[PATH_MAX];
 void print_prompt() {
 	printf("--[%d|%s@%s:~%s]--$ ", command_number, username, hostname, cwd);
 	fflush(stdout);
+}
+
+
+int string_ends_with(const char * str, const char * suffix)
+{
+  int str_len = strlen(str);
+  int suffix_len = strlen(suffix);
+
+  return 
+    (str_len >= suffix_len) &&
+    (0 == strcmp(str + (str_len-suffix_len), suffix));
 }
 
 
@@ -63,12 +79,11 @@ void clean_common(char *tokens[]) {
 	}
 }
 
-bool parse_line(char *line, char *tokens[], int *pipe_ptr, int *token_ptr) {
+void parse_line(char *line, char *tokens[], int *pipe_ptr, int *token_ptr, bool *redirection, bool *background) {
 
 	char *next_tok = line;
 	char *curr_tok;
 	int i = 0;
-	bool redirection = false;
 
 	while(i < 4095 && ((curr_tok = next_token(&next_tok, " \t\r\n")) != NULL)) {
 		if(strncmp(curr_tok, "#", 1) == 0) {
@@ -78,7 +93,7 @@ bool parse_line(char *line, char *tokens[], int *pipe_ptr, int *token_ptr) {
 			(*pipe_ptr)++;
 			tokens[i] = (char *) 0;
 		} else if(strcmp(curr_tok, ">") == 0) {
-			redirection = true;
+			*redirection = true;
 			tokens[i] = (char *) 0;
 		} else {
 			tokens[i] = curr_tok;
@@ -88,10 +103,20 @@ bool parse_line(char *line, char *tokens[], int *pipe_ptr, int *token_ptr) {
 
 	tokens[i] = (char *) 0;
 	(*token_ptr) = i;
-	return redirection;
+	// printf("str is |%s|\n", line);
+	if (i > 0 && strcmp(tokens[i-1], "&") == 0) {
+	// if(string_ends_with(line, "&")) {
+		*background = true;
+		// printf("str is %s\n", tokens[i-1]);
+		tokens[i-1] = (char *) 0;
+		
+		(*token_ptr) = i - 1;
+
+	}
+
 }
 
-void parse_piple(char *tokens[], int *token_num, struct command_line cmds[], bool output){
+void parse_piple(char *tokens[], int token_num, struct command_line cmds[], bool output){
 
 	int i = -1;
 	int cmds_index = 0;
@@ -99,7 +124,7 @@ void parse_piple(char *tokens[], int *token_num, struct command_line cmds[], boo
 	char *curr_tok = NULL;
 	if(!output) {
 
-		while(i < (*token_num)) {
+		while(i < token_num) {
 			//not reach to the end
 			if(curr_tok == NULL) {
 				char **cmd_tokens = tokens+(i+1);
@@ -115,7 +140,7 @@ void parse_piple(char *tokens[], int *token_num, struct command_line cmds[], boo
 
 		cmds[cmds_index-1].stdout_pipe = false;
 	} else {
-		while(i < (*token_num)-2) {
+		while(i < (token_num-2)) {
 			//not reach to the end
 			if(curr_tok == NULL) {
 				char **cmd_tokens = tokens+(i+1);
@@ -129,7 +154,7 @@ void parse_piple(char *tokens[], int *token_num, struct command_line cmds[], boo
 			curr_tok = tokens[i];
 		}
 		cmds[cmds_index-1].stdout_pipe = false;
-		cmds[cmds_index-1].stdout_file = tokens[*token_num-1];
+		cmds[cmds_index-1].stdout_file = tokens[token_num-1];
 	}
 }
 
@@ -160,6 +185,31 @@ void sigint_handler(int signo) {
 	// exit(0);
 }
 
+void move(int index) {
+	for(; index < list_index; index++) {
+		pid_list[index] = pid_list[index+1];
+		line_list[index] = line_list[index+1];
+	}
+}
+
+void sigchld_handler(int signo) {
+	int status;
+	pid_t child = waitpid(-1, &status, WNOHANG);
+	// while (child = waitpid(-1, &status, WNOHANG) != 0) {
+
+
+
+	int i = 0;
+	for(i = 0; i < list_index; i++) {
+		if(pid_list[i] == child) {
+			list_index--;
+			move(i);
+			break;
+		}
+	// }
+}
+}
+
 void history(char *line) {
 	char* command = malloc(sizeof(char));
 	if(line[1] > 47 && line[1] < 58) {
@@ -186,6 +236,7 @@ void history(char *line) {
 int main(void) {
 
 	// signal(SIGINT, sigint_handler);
+	signal(SIGCHLD, sigchld_handler);
 
 	command_number = 0;
 
@@ -195,6 +246,7 @@ int main(void) {
 	short_cwd();
 
 	while(true) {
+		list_index = 0;
 		if (isatty(STDIN_FILENO)) {
 			print_prompt();
 		}
@@ -203,7 +255,6 @@ int main(void) {
 		size_t line_sz = 0;
 
 		ssize_t sz = getline(&line, &line_sz, stdin);
-		// printf("%s\n", line);
 
 		if(sz == EOF || sz == 0) {
 			break;
@@ -220,12 +271,12 @@ int main(void) {
 
 		int total_pipe = 0;
 		int token_num = 0;
-		int *pipe_ptr = &total_pipe;
-		int *token_ptr = &token_num;
+		bool output = false;
+		bool background = false;
 
-		bool output = parse_line(line, tokens, pipe_ptr, token_ptr);
+		parse_line(line, tokens, &total_pipe, &token_num, &output, &background);
 		struct command_line cmds[total_pipe	+1];
-		parse_piple(tokens, token_ptr, cmds, output);
+		parse_piple(tokens, token_num, cmds, output);
 
 		if(tokens[0] == NULL) {
 			continue;
@@ -238,25 +289,56 @@ int main(void) {
 			cd_command(tokens);
 		}
 		if(strcmp(tokens[0], "setenv") == 0) {
-			env_command(tokens, token_ptr);
+			env_command(tokens, &token_num);
 		}
 		if(strcmp(tokens[0], "history") == 0) {
 			print_history();
 			continue;
 		}
 
-		pid_t pid = fork();
-		if(pid == 0) {
-			//child
-			execute_pipeline(cmds);
-			fclose(stdin);
-		} else if (pid == -1) {
-			perror("fork");
-		} else {
-			//parent
-			int status;
-			waitpid(pid, &status, 0);
-		}
+		// printf("bool is %d\n", background);
+			pid_t pid = fork();
+			if(pid == 0) {
+				//child
+				execute_pipeline(cmds);
+				fclose(stdin);
+			} else if (pid == -1) {
+				perror("fork");
+			} else {
+				//parent
+				if(!background){
+					int status;
+					waitpid(pid, &status, 0);
+				} else {
+					pid_list[list_index] = pid;
+					// printf("cmd is %s\n", cmds[0].tokens[0]);
+					// printf("cmd is %s\n", cmds[0].tokens[1]);
+					// printf("cmd is %s\n", cmds[0].tokens[3]);
+					char line_str[strlen(line) + 1];
+					strcpy(line_str, line);
+					line_list[list_index] = line_str;
+					list_index++;
+				}
+			}
+			// //background job
+			// pid_t pid = fork();
+			// if(pid == 0) {
+			// 	//child
+			// 	execute_pipeline(cmds);
+			// 	fclose(stdin);
+			// } else if (pid == -1) {
+			// 	perror("fork");
+			// } else {
+			// 	//parent
+			// 	pid_list[list_index] = pid;
+			// 	char line_str[strlen(line)];
+			// 	strcpy(line_str, line);
+			// 	line_list[list_index] = line_str;
+			// 	list_index++;
+			// 	// int status;
+			// 	// waitpid(pid, &status, 0);
+			// }
+		
 		free(line);
 	}
 
